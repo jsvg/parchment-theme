d55567f9067bdc72bd8bdb25877ab74aaa8407f6
jsvg <vangiessen_julian@bah.com>
2016-01-22 12:24:30 -0500

    content

---
+++ assets/js/parchment.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 150,
+    outDuration: 150,
+    linkElement: 'a', // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+    loading: false, // true shows loading animation/class
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '<img src="../fonts/paper1.png" />', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+});
